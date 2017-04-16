f942fc817d66d9e0db50557a44faa5920927ba93
默默2号 <2>
2017-04-17 00:39:59 +0800

    data

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var vm = new Vue({
+	el:"body",
+	data:"",
+	beforeCreate:function(){
+		//$('.main').html("正在渲染")
+	},
+	mounted:function(){
+		this.$nextTick(function(){
+			
+		})
+	},
+	methods:{
+		
+	}
+})
