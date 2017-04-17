af47c3516002a7b75971dd3b9cdd2627060b7cf1
默默2号 <2>
2017-04-17 22:04:56 +0800

    reader

--- js/index.js
+++ js/index.js
@@ -1,54 +1,11 @@
 var myScroll;
 $(function(){
-	myScroll = new IScroll('#main',{ 
-		probeType: 3,
-		mouseWheel: true,
-        preventDefault: false,
-        fadeScrollbars: true
-	});
-	document.addEventListener('touchmove', function (e) {
-		e.preventDefault(); 
-	}, false);
-	document.addEventListener('mousewheel', function (e) {
-		myScroll.refresh();
-	}, false);
-	var pullDown = $('#PullDown');
-	var pullUp = $('#PullUp');
-	var isPulled = false;
-	myScroll.on('scroll', function() {
-		
-	    var height = this.y,bottomHeight = this.maxScrollY - height;
-
-        // 控制下拉显示
-        if (height >= 30) {
-        	pullDown.show();
-        	setTimeout(function(){
-        		pullDown.hide();
-        	},1500)
-            isPulled = true;
-            return;
-        }
-
-        // 控制上拉显示
-        if (bottomHeight >= 30) {
-            pullUp.show();
-            vm.$data.limit =8;
-            myScroll.refresh()
-            isPulled = true;
-            return;
-        }
-	})
-
-	myScroll.on('scrollEnd', function() { // 滚动结束
-	    if (isPulled) { // 如果达到触发条件，则执行加载
-	        isPulled = false;
-	        
-	    }
-	});	
+	
 })
 window.vm = new Vue({
 	el:'#root',//app元素中所有的元素都能被vue操控
 	data:{
+		show:true,
 		hotBookList:{
 			title:"",
 			data:[],
@@ -85,6 +42,63 @@ window.vm = new Vue({
 	mounted:function(){//实例化编译完成后默认要执行的方法
 		this.$nextTick(function(){//代码保证this.$el在document中
 			this.initBookList();
+			myScroll = new IScroll('#main',{ 
+				probeType: 3,
+				mouseWheel: true,
+		        preventDefault: false,
+		        fadeScrollbars: true
+			});
+			document.addEventListener('touchmove', function (e) {
+				e.preventDefault(); 
+			}, false);
+			document.addEventListener('mousewheel', function (e) {
+				myScroll.refresh();
+			}, false);
+			var pullDown = $('#PullDown');
+			var pullUp = $('#PullUp');
+			var isPulled = false;
+			myScroll.on('scroll', function() {
+				
+			    var height = this.y,bottomHeight = this.maxScrollY - height;
+				
+		        // 控制下拉显示
+		        if (height >= 30) {
+		        	pullDown.show();
+		        	
+		        	setTimeout(function(){
+		        		pullDown.hide();
+		        	},1500)
+		            isPulled = true;
+		            return;
+		        }
+		
+		        // 控制上拉显示
+		        if (bottomHeight >= 30) {
+		            pullUp.show();
+		            setTimeout(function(){
+		            	vm.specialLimit+=5;
+		            	if(vm.specialLimit==vm.special.data.length){
+		            		pullUp.innerHTML="暂无数据";
+		            		return;
+		            	}
+		            	if(vm.specialLimit+5>=vm.special.data.length){
+		            		vm.specialLimit=vm.special.data.length;	
+		            	}else{
+		            		vm.specialLimit+=5;
+		            	}
+		            	pullUp.hide();
+		        		myScroll.refresh();
+		        	},5000)
+		            isPulled = true;
+		            return;
+		        }
+			})
+			myScroll.on('scrollEnd', function() { // 滚动结束
+			    if (isPulled) { // 如果达到触发条件，则执行加载
+			        isPulled = false;
+			        vm.$forceUpdate()
+			    }
+			});	
 		})
 	},
 	computed:{
@@ -101,7 +115,7 @@ window.vm = new Vue({
 			return this.free.data.slice(0,6);
 		},
 		filterspecial:function(){
-			return this.free.data.slice(0,this.specialLimit);
+			return this.special.data.slice(0,this.specialLimit);
 		}
 	},
 	methods:{//所有事件的绑定
@@ -130,20 +144,19 @@ window.vm = new Vue({
 					dataCount:list[4].data.count,
 				}
 				this.free={
-					title:list[4].ad_name,
-					data:list[4].data.data,
-					dataCount:list[4].data.count,
+					title:list[5].ad_name,
+					data:list[5].data.data,
+					dataCount:list[5].data.count,
 				}
 				this.special={
-					title:list[4].ad_name,
-					data:list[4].data.data,
-					dataCount:list[4].data.count,
+					title:list[3].ad_name,
+					data:list[3].data.data,
+					dataCount:list[3].data.count,
 				}
 			})
 		},
 		init:function(event){
 			alert(111)
-		}
-		
+		},
 	}
 })
